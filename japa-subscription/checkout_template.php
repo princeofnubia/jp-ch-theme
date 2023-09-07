f015c8ffb974fe54e301a18cbe71e60b62808b68
Oyewo A O <abubakrilaitan@gmail.com>
2023-09-07 03:19:07 -0500

    seven

---
+++ japa-subscription/checkout_template.php
@@ -0,0 +1,70 @@
+<div class="list-group">
+  <h4>Checkout </h4>
+  <div class="d-flex justify-content-between align-items-center">
+    <h6>Subscribed Content </h6>
+    <a href="#" id="edit-preference">edit</a>
+  </div>
+
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start" id="subscription-relocation-region">
+    <div class="d-flex w-100 justify-content-between">
+      <div>
+        <div class="mb-1 pt-2 ">Relocation region</div>
+        <small data-preference="relocation-region">a</small>
+      </div>
+      <div class="mb-1 checkout-option">
+        <div class="d-flex flex-column edit-subscription hidden" data-preference="relocation-region" data-edit> <i class="fas fa-pencil-alt pencil"></i><small class="small">Edit</small></div>
+
+        <div class="d-flex flex-column remove-subscription hidden" data-preference="relocation-region" data-edit><i class="fas fa-times close"></i><small class="small"  >Remove </small></div>
+      </div>
+    </div>
+
+  </a>
+  <a href="#" class="list-group-item list-group-item-action flex-column mt-2 border-top align-items-start" id="subscription-scholarships">
+    <div class="d-flex w-100 justify-content-between">
+      <div>
+        <div class="mb-1 pt-2 ">Scholarships</div>
+        <small class="text-muted" data-preference="scholarship">Financial aids</small>
+      </div>
+      <div class="mb-1 checkout-option">
+        <div class="d-flex flex-column edit-subscription hidden" data-preference="scholarships" data-edit> <i class="fas fa-pencil-alt pencil"></i><small class="small">Edit</small></div>
+
+        <div class="d-flex flex-column remove-subscription hidden" data-preference="scholarships" data-edit><i class="fas fa-times close"></i><small class="small">Remove</small></div>
+      </div>
+    </div>
+</a>
+
+  <a href="#" class="list-group-item list-group-item-action flex-column mt-2 border-top align-items-start" id="subscription-jobs-internship">
+    <div class="d-flex w-100 justify-content-between">
+      <div>
+        <div class="mb-1 pt-2 ">Jobs and Internships</div>
+        <small class="text-muted" data-preference="scholarship">Financial aids</small>
+      </div>
+      <div class="mb-1 checkout-option">
+        <div class="d-flex flex-column edit-subscription hidden" data-preference="jobs-internship" data-edit > <i class="fas fa-pencil-alt pencil"></i><small class="small">Edit</small></div>
+
+        <div class="d-flex flex-column remove-subscription hidden" data-preference="jobs-internship" data-edit ><i class="fas fa-times close"></i><small class="small">Remove</small></div>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action flex-column mt-2 border-top  align-items-start" id="subscription-programs">
+    <div class="d-flex w-100 justify-content-between">
+      <div>
+        <div class="mb-1 pt-2 ">Programs</div>
+        <small data-preference="relocation-region">yuo</small>
+      </div>
+      <div class="mb-1 checkout-option">
+        <div class="d-flex flex-column edit-subscription hidden" data-preference="programs" data-edit > <i class="fas fa-pencil-alt pencil"></i><small class="small">Edit</small></div>
+
+        <div class="d-flex flex-column remove-subscription hidden" data-preference="programs" data-edit ><i class="fas fa-times close"></i><small class="small">Remove </small></div>
+      </div>
+    </div>
+
+  </a>
+  <h4>Subscription</h4>
+  <a href="#" class="list-group-item list-group-item-action flex-column border-top align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <div class="mb-1 pt-2 ">N 100 /wk </div>
+      <div class="d-flex flex-column hidden"  data-edit> <i class="fas fa-pencil-alt pencil"></i><small class="small">Edit</small></div>
+    </div>
+  </a>
+</div>
